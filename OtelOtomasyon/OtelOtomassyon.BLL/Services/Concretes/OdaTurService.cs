58784aa80b3ec009cbe5002ddf1a8de8c2fd2d76
Taner İldan <tanerildan@gmail.com>
2018-01-06 13:16:14 +0300

    Service Düzenlendi

---
+++ OtelOtomasyon/OtelOtomassyon.BLL/Services/Concretes/OdaTurService.cs
@@ -0,0 +1,19 @@
+﻿using OtelOtomassyon.BLL.Services.Abstracts;
+using OtelOtomasyon.Repository.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OtelOtomassyon.BLL.Services.Concretes
+{
+   public class OdaTurService:IOdaTurService
+    {
+        private readonly IOdaTurRepository _odaTurRepository;
+        public OdaTurService(IOdaTurRepository odaTurRepository)
+        {
+            _odaTurRepository = odaTurRepository;
+        }
+    }
+}
